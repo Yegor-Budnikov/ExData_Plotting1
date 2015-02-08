2996fc413413a7be4adb45781c92453afebc44a1
Yegor-Budnikov <yegor.budnikov@gmail.com>
2015-02-08 22:21:15 +0300

    modified

--- plot4.R
+++ plot4.R
@@ -5,14 +5,26 @@ rm(alldata)
 data$Time <- strptime(as.character(paste(data[,1], data[,2])), format="%d/%m/%Y %H:%M:%S")
 
 Sys.setlocale("LC_TIME", "English")
+par(mfrow=c(2,2))
+
+plot(data$Time, data$Global_active_power, xlab = "", ylab = "Global Activity Power", type = "n")
+lines(data$Time, data$Global_active_power)
+
+plot(data$Time, data$Voltage, xlab = "datatime", ylab = "Voltage", type = "n")
+lines(data$Time, data$Voltage)
+
 plot(data$Time, data$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "n")
 lines(data$Time, data$Sub_metering_1, col="black")
 lines(data$Time, data$Sub_metering_2, col="red")
 lines(data$Time, data$Sub_metering_3, col="blue")
-legend("topright", lty=1, col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty=1, col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lwd = .75, cex = .75)
+
+plot(data$Time, data$Global_reactive_power, xlab = "datatime", ylab = "Global_reactive_power", type = "n")
+lines(data$Time, data$Global_reactive_power)
 
 
-dev.copy(png, file = "./data/plot3.png")
+
+dev.copy(png, file = "./data/plot4.png")
 dev.off()
 
 
